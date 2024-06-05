ca941f028d4ba2de262780565310b04dd7b181d6
WarinCode <warinsaipanya471@outlook.co.th>
2024-06-05 21:26:35 +0700

    updated MultiplicationTable.java

--- src/MultiplicationTable.java
+++ src/MultiplicationTable.java
@@ -4,99 +4,105 @@ import java.awt.event.*;
 import javax.swing.*;
 
 /* สร้าง class ตารางแม่สูตรคูณ
-* ใช้ interface MouseListener เพื่อมา implement ให้ method เพื่อจัดการกับ event การคลิกของปุ่ม
+* ใช้ interface MouseListener และ WindowListener เพื่อมา implement ให้แต่ละ method เพื่อจัดการกับ event ในโปรแกรม
 * ใช้ interface SwingConstants เพื่อจะใช้ attribute ที่เป็นค่าคงทีสำหรับจัดการตำแหน่งของข้อความ */
-public class MultiplicationTable implements MouseListener, SwingConstants {
-    // ประกาศ attributes ของ components
+public final class MultiplicationTable implements WindowListener, MouseListener, SwingConstants {
+    // ประกาศ attributes ของ components ทั้งหมด
     private final JFrame frame;
     private final Container content;
     private JButton btn;
     private JLabel label;
-    private JLabel label2;
-    private JPanel panel;
-    private JPanel panel2;
+    private JLabel title;
+    private JPanel topPanel;
+    private JPanel mainPanel;
     private JTextField field;
 
-    // constructor เมื่อสร้าง object ให้ทำการสร้างหน้าต่างโปรแกรมและวาง component ทันที
+    // constructor เมื่อสร้าง object ให้ทำการสร้างหน้าต่างโปรแกรมและวาง components ทันที
     public MultiplicationTable(){
         // กำหนดค่าเริ่มต้นให้ attributes เป็น object ของแต่ละ component
         this.frame = new JFrame("Multiplication Table");
         this.content = this.frame.getContentPane();
         this.btn = new JButton("Click here");
-        this.panel = new JPanel();
-        this.panel2 = new JPanel();
+        this.topPanel = new JPanel();
+        this.mainPanel = new JPanel();
         this.field = new JTextField();
         this.label = new JLabel("Enter number: ");
-        this.label2 = new JLabel();
+        this.title = new JLabel();
 
-        // กำหนดขนาดหน้าต่าง, ตำแหน่งการวางโปรแกรมบนหน้าจอ และ ใช้ layout เป็นแบบ border
+        // กำหนดขนาดหน้าต่าง, ตำแหน่งการวางโปรแกรมบนหน้าจอ และใช้ layout เป็นแบบ border
         this.frame.setLayout(new BorderLayout());
         this.frame.setVisible(true);
         this.frame.setSize(300, 430);
         this.frame.setLocation(530, 100);
-        // สร้าง object ด้านใน arguemnt เมื่อมีการคลิกที่ปุ่มปิดโปรแกรมๆจะจบการทำงาน
-        this.frame.addWindowListener(new WindowAdapter() {
-            public void windowClosing(WindowEvent e) {
-                System.exit(0);
-            }
-        });
+        // ส่ง this keyword เป็น argument หมายถึงให้ใช้ class นี้เป็น listener เมื่อมี event เกิดขึ้นในโปรแกรมจะเรียกใช้ method ที่ implement มาจาก interface
+        this.frame.addWindowListener(this);
+        this.btn.addMouseListener(this);
 
         // ให้ panels ใช้ layout เป็นแบบ grid และกำหนด แถว และ คอลัมน์
-        this.panel.setLayout(new GridLayout(3,1));
-        this.panel2.setLayout(new GridLayout(13, 1));
+        this.topPanel.setLayout(new GridLayout(3,1));
+        this.mainPanel.setLayout(new GridLayout(13, 1));
         // ให้ panel เพิ่ม components และวาง panel ในส่วนตำแหน่งด้านบ้านโปรแกรม
-        this.panel.add(this.label);
-        this.panel.add(this.field);
-        this.panel.add(this.btn);
-        this.content.add(panel, BorderLayout.NORTH);
+        this.topPanel.add(this.label);
+        this.topPanel.add(this.field);
+        this.topPanel.add(this.btn);
+        this.content.add(topPanel, BorderLayout.NORTH);
 
         // ให้ textField มี cursor กระพิบที่ช้องป้อนข้อความ
         this.field.grabFocus();
-        // เมื่อมี event กับการคลิกปุ่มให้เรียกใช้ method ที่ทำการ implement มาคือ interface MouseListener
-        this.btn.addMouseListener(this);
     }
 
-    public void generateNumbers(int n){
-        // ลบ components ทั้งหมดที่อยู่ใน panel2 ออกก่อนแล้วค่อยวาง components ใหม่เข้าไป
-        this.panel2.removeAll();
-        this.panel2.setVisible(false);
+    private void generateNumbers(int n){
+        // ลบ components ทั้งหมดที่อยู่ใน main panel ออกก่อนแล้วค่อยวาง components ใหม่เข้าไป
+        this.mainPanel.removeAll();
+        this.mainPanel.setVisible(false);
         this.field.setText("");
         // ใส่ข้อความ , จัดตำแหน่ง , เปลี่ยนสี และ เพิ่ม component เข้าไปใน panel2
-        this.label2.setText("The Multiplication Table of: " + n);
-        this.label2.setForeground(Color.BLUE);
-        this.label2.setHorizontalAlignment(this.CENTER);
-        this.panel2.add(this.label2);
-        // วน loop เพิ่ม components และนำ panel2 ไปใส่ใน contentpane อีกที
+        this.title.setText("The Multiplication Table of: " + n);
+        this.title.setForeground(Color.BLUE);
+        this.title.setHorizontalAlignment(this.CENTER);
+        this.mainPanel.add(this.title);
+        String text = null;
+        // วน loop เพิ่ม components และนำ main panel ไปใส่ใน content pane อีกที
         for(int i = 1; i <= 12; i++){
+            text = n + " x " + i + " = " + n * i;
             JLabel label = new JLabel();
             label.setHorizontalAlignment(this.CENTER);
-            label.setText(n + " x " + i + "= " + n * i);
-            this.panel2.add(label);
+            label.setText(text);
+            this.mainPanel.add(label);
         }
     }
 
+    // implement methods ทั้งหมด
     public void mouseClicked(MouseEvent e) {
         try {
             // ถ้าช้องป้อนข้อความไม่ได้ใส่ข้อความมา
             if(this.field.getText().isBlank()){
-                this.showErrorMessage("Invalid number! Please enter a number again.");
+                this.showErrorMessage("Invalid number, Please enter a number again.");
             } else {
                 // แปลงข้อความที่ได้เป็นเลขจำนวนเต็มและส่ง argument ให้ method ไปทำงาน
                 this.generateNumbers(Integer.parseInt(this.field.getText()));
-                // เอา panel2 มาใส่ใน contentpane และจัดตำแหน่งให้วางตรงกลางของเนื้อหา
-                this.content.add(this.panel2, this.CENTER);
-                this.panel2.setVisible(true);
+                // เอา main panel มาใส่ใน content pane และจัดตำแหน่งให้วางตรงกลางของเนื้อหา
+                this.content.add(this.mainPanel, this.CENTER);
+                this.mainPanel.setVisible(true);
             }
-        } catch(Exception error){
-            // ใช้ class Exception เมื่อเกิดข้อผิดพลาดที่แปลงจากข้อความไปเป็นเลขจำนวนเต็มไม่สำเร็จให้แสดง dialog ที่มีข้อความ error
-            this.showErrorMessage("Error: " + error.getMessage());
+        } catch(NumberFormatException exception){
+            // ใช้ class NumberFormatException เมื่อเกิดข้อผิดพลาดที่แปลงจากข้อความไปเป็นเลขจำนวนเต็มไม่สำเร็จให้แสดง dialog ที่มีข้อความ error ขึ้นมา
+            this.showErrorMessage("Error: " + exception.getMessage());
         }
     }
-
     public void mouseEntered(MouseEvent e) {}
     public void mouseExited(MouseEvent e) {}
     public void mousePressed(MouseEvent e) {}
     public void mouseReleased(MouseEvent e) {}
+    public void windowOpened(WindowEvent e){}
+    public void windowClosing(WindowEvent e){
+        System.exit(0);
+    }
+    public void windowClosed(WindowEvent e){}
+    public void windowIconified(WindowEvent e){}
+    public void windowDeiconified(WindowEvent e){}
+    public void windowActivated(WindowEvent e){}
+    public void windowDeactivated(WindowEvent e){}
 
     // แสดงข้อความ error ที่เกิดขึ้น
     public void showErrorMessage(String text){
